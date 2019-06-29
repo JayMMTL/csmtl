7dfc713befbbb5231619ad94466071fcfe01e9a0
Jay Filiatrault <jaymmtl@Jays-MacBook-Pro.local>
2019-06-29 17:39:53 -0400

    update(JSON package files & new logo width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="CS-MTL" style={{ width: '75px' }} />
+              <img src={logo} alt="CS-MTL" style={{ width: '50px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
