efc2610d9312d61beb82ef991a877e661e575b40
Jay Filiatrault <jaymmtl@Jays-MacBook-Pro.local>
2019-06-29 22:19:41 -0400

    fix(logo color)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="CS-MTL" style={{ width: '88px' }} />
+              <img src={logo} alt="CS-MTL" style={{ width: '88px', fill: {$primary}, color: {$primary} }} />
             </Link>
             {/* Hamburger menu */}
             <div
